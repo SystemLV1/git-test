90085b8524e4520bd145efec47a720c149b0557d
轩辕东起 <xydq@cc.com>
2022-03-27 23:31:39 +0800

    纠正错误

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,7 +16,7 @@ public class GitTest {
 
         System.out.println("你是不是傻子");
         System.out.println("你是魔鬼中的天使，所以送我心碎的方式");
-        Sysetm.out.println("如果那天你不知道我喝了多少杯");
+        System.out.println("如果那天你不知道我喝了多少杯");
         System.out.println("江湖啊，江湖，社会呀，社会");
     }
 }
