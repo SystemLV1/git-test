bc79a9148598b177af70b86ddbc37e560e3c9533
SystemLV1 <102458469+SystemLV1@users.noreply.github.com>
2022-03-27 23:07:42 +0800

    Update GitTest.java
    才会在刹那之间只想和你一起到白头

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,11 +6,13 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("我承认都是月亮惹的祸，那样的月色太美你太温柔");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("pull test!");
         
         System.out.println("吃不了学习的苦，就要吃没钱的苦");
+        System.out.println("你是魔鬼中的天使，所以送我心碎的方式");
     }
 }
